ffc4aa4fee1f8f42e1be4675a98abe21453d80a9
alyxandria meadows <alyxmeadows@yahoo.com>
2021-07-31 12:11:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+  function writeCards(array, event) {
+      const cards = []
+      for (let i = 0; i < array.length; i++) {
+          cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        }
+        return cards;
+      }
+
+function countDown() {
+      let countDown = 10;
+      while (countDown > -1) {
+        console.log(countDown--);
+      } 
+    }
